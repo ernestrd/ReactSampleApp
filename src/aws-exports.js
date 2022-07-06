1bf179776e451df6f257a264cb43a70e1c433184
ernestrd <ernestrd@amazon.com>
2022-07-06 13:20:22 -0500

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,7 +3,7 @@
 const awsmobile =  {
     "aws_appsync_graphqlEndpoint": "https://jji4nbll6zgrvej4klbjngwoqe.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
-    "aws_appsync_authenticationType": "AWS_IAM",
+    "aws_appsync_authenticationType": "API_KEY",
     "aws_appsync_apiKey": "da2-2peqzxouzngr7hivfbb36eyepe",
 };
 
